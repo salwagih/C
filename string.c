aa39a8a442bff99cf0a1ff992acc58e3a1a5368c
salwagih <101877626+salwagih@users.noreply.github.com>
2022-06-07 00:43:29 +0200

    Update string.c

--- string.c
+++ string.c
@@ -1,15 +1,22 @@
 #include <cs50.h>
 #include <stdio.h>
 
+
+int string_length(string s);
 int main (void)
 {
-    string name = get_string("name: ");
-    
+    string name = get_string("Name: ");
+    int length = string_length(name);
+    printf("%i" , length);
+       
+}
+
+int string_length(string s)
+{
     int i = 0;
-    while (name[i] != '\0')
+    while (s[i] != '\0')
     {
       i++;
     }
-  
-printf("%i" , i);
+  return i;
 }
